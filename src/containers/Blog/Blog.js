b93381b84050a26bceb1f3ef7920d65f4c0ef5fe
Fabio De Oliveira Franco <fabioof@br.ibm.com>
2021-04-12 22:46:25 -0300

    Load routes using guards and lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
 // import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
+    state = {
+        auth: true
+    }
+
     render() {
-        
         return (
             <div className="Blog">
                 <header>
@@ -35,8 +43,10 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    { this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                     <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
                 </Switch>
             </div>
         );
